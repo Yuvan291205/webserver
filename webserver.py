16735943d9c054b9c26f1d3f1df16070edd7373e
Yuvan291205 <yuvvanmuthu@gmail.com>
2023-07-13 10:20:40 +0530

    first commit

--- webserver.py
+++ webserver.py
@@ -1,34 +1,5 @@
-# Developing a Simple Webserver
-
-# AIM:
-
-To develop a simple webserver to serve html programming pages.
-
-## DESIGN STEPS:
-
-### Step 1:
-
-HTML content creation is done
-
-### Step 2:
-
-Design of webserver workflow
-
-### Step 3:
-
-Implementation using Python code
-
-### Step 4:
-
-Serving the HTML pages.
-
-### Step 5:
-
-Testing the webserver
-
-## PROGRAM:
 from http.server import HTTPServer,BaseHTTPRequestHandler
-```py
+
 content='''
 <!doctype html>
 <html>
@@ -40,7 +11,7 @@ content='''
 <h2>1.Django</h2>
 <h2>2. MEAN Stack</h2>
 <h2>3. React </h2>
-<h2>4. bootstrap </h2>
+<h2>4. Bootstrap </h2>
 <h2>5. React </h2>
 </body>
 </html>
@@ -55,14 +26,6 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(content.encode())
 
 print("This is my webserver") 
-server_address =('',80)
+server_address =('',8000)
 httpd = HTTPServer(server_address,MyServer)
 httpd.serve_forever()
-```
-## OUTPUT:
-###server output
-![server output](serveroutput.jpg)
-###client output
-![client output](clientoutput.jpg)
-## RESULT:
-The program is executed succesfully
